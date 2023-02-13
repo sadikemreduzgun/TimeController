6f787f15c418fc05134f46843b492ff805787a6e
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-02-13 13:31:10 +0200

    Update inter.py

--- src/inter.py
+++ src/inter.py
@@ -1,14 +1,12 @@
-with open('keep.txt', 'r') as f:
+with open('../data/keep.txt', 'r') as f:
     start_date = f.read()
     start_date = start_date[0:5]
     
-
     f.close()
-
 
 print(start_date)
 
-with open('keep.txt', 'w') as f:
+with open('../data/keep.txt', 'w') as f:
 
     f.write(start_date)
     f.close()
