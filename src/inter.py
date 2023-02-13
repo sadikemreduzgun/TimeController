15ee8bb1adfb1d42d633de8cfee3df78fa78b0d2
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-02-13 13:30:12 +0200

    Create inter.py

---
+++ src/inter.py
@@ -0,0 +1,15 @@
+with open('keep.txt', 'r') as f:
+    start_date = f.read()
+    start_date = start_date[0:5]
+    
+
+    f.close()
+
+
+print(start_date)
+
+with open('keep.txt', 'w') as f:
+
+    f.write(start_date)
+    f.close()
+    
