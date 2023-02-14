0aeed520a9a3e7f07ba5448e970fbb042e434e52
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-02-14 09:49:12 +0200

    Create settings.sh

---
+++ src/settings.sh
@@ -0,0 +1,13 @@
+# time settings
+max_hour=1
+max_minute=0
+
+# if it is slowing down, increase it
+control_time=15
+
+# determine which software it is
+software=firefox
+
+# don't change if you don't want spamming messages
+bool=1
+bool2=1
