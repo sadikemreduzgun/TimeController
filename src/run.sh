48b643d6e6eadffb54d0c088051094bfd0ec24dd
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-02-14 09:11:36 +0200

    Create run.sh

---
+++ src/run.sh
@@ -0,0 +1,12 @@
+# make it automaticly executed when system start.
+sudo touch /etc/rc.local
+
+sudo touch /etc/systemd/system/rc-local.service
+sudo chmod +x /etc/rc.local
+
+# get actual username not "root".
+user=$(python3 get_user.py)
+
+sudo echo "/home/$user/Downloads/StartScript.sh &" > /etc/rc.local
+
+sudo bash controller.sh
